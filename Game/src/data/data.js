d2b3491839443891fd866251b5f3147cf4b536df
Your Name <you@example.com>
2024-09-11 13:56:00 -0500

    data

--- Game/src/data/data.js
+++ Game/src/data/data.js
@@ -19,6 +19,31 @@ const questionsData = {
             questionText: "¿Cuál es el idioma más hablado en el mundo?",
             options: ["Inglés", "Mandarín", "Español", "Hindú"],
             correctAnswer: "Mandarín"
+        },
+        {
+            questionText: "¿Quién pintó 'La Última Cena'?",
+            options: ["Leonardo da Vinci", "Van Gogh", "Pablo Picasso", "Claude Monet"],
+            correctAnswer: "Leonardo da Vinci"
+        },
+        {
+            questionText: "¿Quién es el autor de 'Don Quijote de la Mancha'?",
+            options: ["Jorge Luis Borges", "Isabel Allende", "Miguel de Cervantes Saavedra", "Gabriel García Márquez"],
+            correctAnswer: "Miguel de Cervantes Saavedra"
+        },
+        {
+            questionText: "¿Quién sabía que no sabia nada?",
+            options: ["Aristóteles", "Sócrates", "Platón", "Descartes"],
+            correctAnswer: "Sócrates"
+        },
+        {
+            questionText: "¿En qué año se lanzó el primer iPhone?",
+            options: ["2004", "2009", "2002", "2007"],
+            correctAnswer: "2007"
+        },
+        {
+            questionText: "¿Qué filósofo griego fue maestro de Alejandro Magno?",
+            options: ["Sócrates", "Aristóteles", "Descartes", "Platón"],
+            correctAnswer: "Aristóteles"
         }
     ],
     "Deporte": [
@@ -41,7 +66,33 @@ const questionsData = {
             questionText: "¿En qué año se celebraron los primeros Juegos Olímpicos modernos?",
             options: ["1896", "1900", "1912", "1924"],
             correctAnswer: "1896"
+        },
+        {
+            questionText: "¿Cómo se llama la posición del jugador con uniforme de color diferente en voleibol?",
+            options: ["Armador", "Libero", "Auxiliar", "Central"],
+            correctAnswer: "Libero"
+        },
+        {
+            questionText: "¿Qué país ha ganado más medallas en los Juegos Olímpicos?",
+            options: ["Japón", "China", "Rusia", "Estados Unidos"],
+            correctAnswer: "Estados Unidos"
+        },
+        {
+            questionText: "¿Qué jugador de baloncesto es conocido como 'Su Majestad'?",
+            options: ["Stephen Curry", "Lebron James", "Michael Jordan", "Kevin Durant"],
+            correctAnswer: "Michael Jordan"
+        },
+        {
+            questionText: "¿Qué selección ganó la primera Copa Mundial de la FIFA en 1930?",
+            options: ["Uruguay", "Argentina", "Brasil", "Chile"],
+            correctAnswer: "Uruguay"
+        },
+        {
+            questionText: "¿Cuál es el deporte que se practica con una pelota y una raqueta en una cancha dividida por una red?",
+            options: ["Badminton", "Ping Pong", "Voleibol", "Tenis"],
+            correctAnswer: "Tenis"
         }
+
     ],
     "Historia": [
         {
@@ -63,7 +114,32 @@ const questionsData = {
             questionText: "¿Qué evento desencadenó la Primera Guerra Mundial?",
             options: ["El hundimiento del Titanic", "El asesinato del archiduque Francisco Fernando", "La Revolución Rusa", "La invasión de Polonia"],
             correctAnswer: "El asesinato del archiduque Francisco Fernando"
-        }
+        },
+        {
+            questionText: "¿En qué conflicto bélico se utilizó por primera vez el tanque?",
+            options: ["Primera Guerra Mundial", "Segunda Guerra Mundial", "Guerra Fría", "Guerra de Vietnam"],
+            correctAnswer: "Primera Guerra Mundial"
+        },
+        {
+            questionText: "¿Qué tratado puso fin a la Primera Guerra Mundial?",
+            options: ["Tratado de Tordesillas", "Tratado de Utrecht", "Tratado de Versalles", "Tratado de Ginebra"],
+            correctAnswer: "Tratado de Versalles"
+        },
+        {
+            questionText: "¿Quién fue el presidente de los Estados Unidos durante la Guerra Civil?",
+            options: ["Thomas Jefferson", "George Washington", "John Adams", "Abraham Lincoln"],
+            correctAnswer: "Abraham Lincoln"
+        },
+        {
+            questionText: "¿En qué año llegó Cristóbal Colón a América?",
+            options: ["1492", "1490", "1500", "1485"],
+            correctAnswer: "1492"
+        },
+        {
+            questionText: "¿Qué país fue el primero en enviar un satélite artificial al espacio?",
+            options: ["Unión Sovietica", "Japón", "Estados Unidos", "Francia"],
+            correctAnswer: "Unión Sovietica"
+        },
     ],
     "Geografía": [
         {
@@ -85,7 +161,43 @@ const questionsData = {
             questionText: "¿Cuál es el país más pequeño del mundo?",
             options: ["Mónaco", "Nauru", "Ciudad del Vaticano", "San Marino"],
             correctAnswer: "Ciudad del Vaticano"
+        },
+        {
+            questionText: "¿En qué continente se encuentra el río Nilo?",
+            options: ["Europa", "África", "Asia", "Oceanía"],
+            correctAnswer: "África"
+        },
+        {
+            questionText: "¿Cuál es el desierto más grande del mundo?",
+            options: ["Antártico", "Kalahari", "Sahara", "Gobi"],
+            correctAnswer: "Antártico"
+        },
+        {
+            questionText: "¿Cuál es la capital de Canada?",
+            options: ["Toronto", "Vancouver", "Montreal", "Ottawa"],
+            correctAnswer: "Ottawa"
+        },
+        {
+            questionText: "¿Qué país tiene la forma de una bota en el mapa?",
+            options: ["España", "Italia", "Chile", "Grecia"],
+            correctAnswer: "Italia"
+        },
+        {
+            questionText: "¿Cómo se llama el lugar del océano más alejado de cualquier tierra firme?",
+            options: ["Punto de Greenwich", "Fosa de las Marianas", "Punto Nemo", "Punto de la Antártida"],
+            correctAnswer: "Punto Nemo"
+        },
+        {
+            questionText: "¿Cuál es el único continente sin desierto?",
+            options: ["América", "Asia", "Oceanía", "Europa"],
+            correctAnswer: "Europa"
+        },
+        {
+            questionText: "¿Cuál es el único continente sin desierto?",
+            options: ["América", "Asia", "Oceanía", "Europa"],
+            correctAnswer: "Europa"
         }
+        
     ],
     "Entretenimiento": [
         {
@@ -107,7 +219,37 @@ const questionsData = {
             questionText: "¿Qué banda lanzó el álbum 'Abbey Road'?",
             options: ["The Beatles", "The Rolling Stones", "Pink Floyd", "Queen"],
             correctAnswer: "The Beatles"
-        }
+        },
+        {
+            questionText: "¿Cuál es el nombre del villano en la película 'El silencio de los inocentes'?",
+            options: ["Norman Bates", "Michael Myers", "Hannibal Lecter", "Freddy Krueger"],
+            correctAnswer: "Hannibal Lecter"
+        },
+        {
+            questionText: "¿Quién interpretó a Jack Dawson en la película 'Titanic'?",
+            options: ["Brad Pitt", "Leonardo DiCaprio", "Matthew McConaughey", "Tom Cruise"],
+            correctAnswer: "Leonardo DiCaprio"
+        },
+        {
+            questionText: "¿Qué director de cine es conocido por películas como 'Pulp Fiction' y 'Reservoir Dogs'?",
+            options: ["Steven Spielberg", "Martin Scorsese", "Alfred Hitchcock", "Quentin Tarantino"],
+            correctAnswer: "Quentin Tarantino"
+        },
+        {
+            questionText: "¿Qué actor ha interpretado a James Bond más veces en el cine?",
+            options: ["Roger Moore", "Sean Connery", "Daniel Craig", "Pierce Brosnan"],
+            correctAnswer: "Roger Moore"
+        },
+        {
+            questionText: "¿Cuál fue la primera película de Disney?",
+            options: ["La Cenicienta", "Blanca Nieves", "La Sirenita", "La Bella y La Bestia"],
+            correctAnswer: "Blanca Nieves"
+        },
+        {
+            questionText: "¿Quién dijo esta frase? 'El pasado puede doler. Pero según lo veo puedes o huir de él o aprender'",
+            options: ["Timón", "Mufasa", "Pumba", "Rafiki"],
+            correctAnswer: "Rafiki"
+        },
     ],
     "Retos": [
         {
@@ -116,7 +258,12 @@ const questionsData = {
             correctAnswer: "Listo"
         },
         {
-            questionText: "Canta una canción completa.",
+            questionText: "Di los nombres de cinco ciudades que empiecen con la letra B en 30 segundos.",
+            options: ["Listo", "No puedo"],
+            correctAnswer: "Listo"
+        },
+        {
+            questionText: "Di el abecedario al revés sin equivocarte.",
             options: ["Listo", "No puedo"],
             correctAnswer: "Listo"
         },
@@ -126,7 +273,7 @@ const questionsData = {
             correctAnswer: "Listo"
         },
         {
-            questionText: "Baila sin música durante 1 minuto.",
+            questionText: "Baila sin música durante 30 segundos.",
             options: ["Listo", "No puedo"],
             correctAnswer: "Listo"
         },
@@ -139,7 +286,12 @@ const questionsData = {
             questionText:"El grupo debe organizarse en orden de estatura, de menor a mayor, en 30 segundos.",
             options: ["Lo logramos", "No logramos"],
             correctAnswer: "Lo logramos"
-        }
+        },
+        {
+            questionText:"Un miembro describe un objeto sin decir su nombre y los demás deben adivinarlo en 30 segundos.",
+            options: ["Lo logramos", "No logramos"],
+            correctAnswer: "Lo logramos"
+        },
     ]
 };
 
